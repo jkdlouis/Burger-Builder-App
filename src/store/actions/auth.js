aef46615b0aa3937ac66c654f3ef1e0b85c349ea
Louis Huang <lhuang5@apple.com>
2019-01-14 19:39:28 -0800

    Fixed redirect issue on front page, and css issue.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -43,7 +43,6 @@ export const auth = (email, password, isSignup) => {
               localStorage.setItem('token', response.data.idToken);
               localStorage.setItem('expirationDate', expirationDate);
               localStorage.setItem('userId', response.data.localId);
-              console.log(response.data.localId);
               dispatch(authSuccess(response.data.idToken, response.data.localId));
               dispatch(checkAuthTimeout(response.data.expiresIn))
           })
